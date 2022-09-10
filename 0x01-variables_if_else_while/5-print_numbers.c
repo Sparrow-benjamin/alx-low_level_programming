9a59341016674ed7e8e9e2b47ffd6bb7cde3fcb7
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-10 14:50:03 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,29 +1,15 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
 
-
-
-/* betty style doc for function main goes there */
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-int main(void)
-  
+int main(void) 
 {
-  
-  printf("0123456789\n");
-  
-  return (0);
-  
+  int i;
+  for (i = 0; i <= 9; ++i)
+    printf("%d", i);
+  printf("\n");
+  return (0); 
 }
