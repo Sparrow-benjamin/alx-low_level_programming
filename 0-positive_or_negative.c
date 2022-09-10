7fd9b8ed4db93b0514f24f98c33d23776a38d6a9
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-09 22:49:59 -0700

    is it positive or negative

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,6 +1,11 @@
+Variables, if, else, while project
 #include <stdlib.h>
 
 #include <time.h>
+
+#include <stdio.h>
+
+
 
 /* betty style doc for function main goes there */
 
@@ -15,31 +20,43 @@
  */
 
 int main(void)
-  
+
 {
-  
-  int n;
-  
-  srand(time(0));
-  
-  n = rand() - RAND_MAX / 2;
-  
-  if (n > 0)
-    
-    {printf("%i is positive\n", n);
-      
-    }
-  
-  else if (n == 0)
-    
-    {printf("%i is zero\n", n);
-      
-    }
-  
-  else if (n < 0)
-    
-    {printf("%i is negative\n", n);
-      
-    }return (0);
-  
+
+int n;
+
+
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+
+{
+
+printf("%i is positive\n", n);
+
+}
+
+else if (n == 0)
+
+{
+
+printf("%i is zero\n", n);
+
+}
+
+else if (n < 0)
+
+{
+
+printf("%i is negative\n", n);
+
+}
+
+
+
+return (0);
+
 }
