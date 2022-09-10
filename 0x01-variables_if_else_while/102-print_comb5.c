351e2dbf362dddd19ba8213e6ef42b7b9eb83996
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-10 07:02:07 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/* betty style doc for function main goes there */
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  int i, j, k, l;
+  
+
+  
+  i = j = k = 48; l = 49;
+  
+  while  ((i < 58))
+    
+    {
+      
+      putchar(i); putchar(j); putchar(32); putchar(k); putchar(l);
+      
+      if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+	
+	{
+	  
+	  putchar('\n'); i++;
+	  
+	}
+      
+      else
+	
+	{
+	  
+	  putchar(44); putchar(32);
+	  
+	  if ((k == 57) && (l == 57))
+	    
+	    {
+	      
+	      if (j < 56)
+		
+		{
+		  
+		  l = ++j + 1; k = i;
+		  
+		}
+	      
+	      else if (j == 56)
+		
+		{
+		  
+		  j++; k = i + 1; l = 48;
+		  
+		}
+	      
+	      else if (j == 57)
+		
+		{
+		  
+		  j = 48; l = 49; k = ++i;
+		  
+		}
+	      
+	    }
+	  
+	  else if (l < 57)
+	    
+	    {
+	      
+	      l++;
+	      
+	    }
+	  
+	  else
+	    
+	    {
+	      
+	      l = 48; k++;
+	      
+	    }
+	  
+	}
+      
+    }
+  
+  return (0);
+  
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
- a program that prints all possible different combinations of three digits
+ a program that prints all possible combinations of two two-digit numbers
