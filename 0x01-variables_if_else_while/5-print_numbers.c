5cf853f6083ce450f0dc809b689b6cd2a1d9916d
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-11 00:53:44 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-  int i;
-  for (i = 0; i <= 9; ++i)
-    printf("%d", i);
-  printf("\n");
-  return (0); 
+int i;
+for (i = 0; i <= 9; ++i)
+printf("%d", i);
+printf("\n");
+return (0); 
 }
