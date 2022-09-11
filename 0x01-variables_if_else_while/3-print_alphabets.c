1f27e274b00d300f5e4a8c24b9c116785f563037
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-11 03:46:53 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,10 @@
  */
 int main(void) 
 {char c;
-  for (c = 'a'; c <= 'z'; ++c)
-    putchar(c);
-  for (c = 'A'; c <= 'Z'; ++c)
-    putchar(c);
-  putchar('\n');
-  return (0); 
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+for (c = 'A'; c <= 'Z'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
 }
