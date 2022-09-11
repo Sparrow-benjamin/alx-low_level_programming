c1c1223427396f70d8b3b5cf856df4638c6722c6
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-11 04:04:43 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,41 +1,15 @@
 #include <stdio.h>
 
-
-
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-  
 {
-  
-  char l;
-  
-
-  
-  l = 'z';
-  
-  while (l >= 'a')
-    
-    {
-      
-      putchar(l);
-      
-      l--;
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
+char c;
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+putchar('\n');
+return (0);
 }
