6ff38aaadeec55eb5b21e74361228ada63b9a3c5
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-11 04:07:41 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,63 +1,18 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
 
-
-
-/* betty style doc for function main goes there */
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-  
 {
-  
-  int i;
-  
-  char l;
-  
-
-  
-  i = 48;
-  
-  while  (i < 58)
-    
-    {
-      
-      putchar(i);
-      
-      i++;
-      
-    }
-  
-
-  
-  l = 'a';
-  
-  while (l <= 'f')
-    
-    {
-      
-      putchar(l);
-      
-      l++;
-      
-    }
-  
-  putchar('\n');
-  
-
-  
-  return (0);
-  
+int i;
+char c;
+for (i = '0'; i <= '9'; ++i)
+putchar(i);
+for (c = 'a'; c <= 'f'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
 }
