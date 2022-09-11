cfafc526c6fe84c358e3c561fb9882fdb32293c4
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-11 04:10:51 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,65 +1,22 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
 
-
-
-/* betty style doc for function main goes there */
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-  
 {
-  
-  int i;
-  
-
-  
-  i = 48;
-  
-  while  (i < 58)
-    
-    {
-      
-      putchar(i);
-      
-      if (i !=  57)
-	
-	{
-	  
-	  putchar(44);
-	  
-	  putchar(32);
-	  
-	  i++;
-	  
-	}
-      
-      else
-	
-	{
-	  
-	  i++;
-	  
-	}
-      
-    }
-  
-  putchar('\n');
-  
-
-  
-  return (0);
-  
+int i;
+for (i = '0'; i <= '9'; ++i)
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
