0ad37df9c57cf2e52d8df0f722aff786f0a90435
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-13 18:53:12 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,35 +1,15 @@
 #include "main.h"
-
 /**
-
  * main - entry point
-
  *
-
  * Return: ALways Success(0)
-
  */
-
-
-
-int main(void)
-  
+int main(void) 
 {
-  
-  char c[8] = "_putchar";
-  
-  int i;
-  
-
-  
-  for (i = 0; i < 8; ++i)
-    
-    _putchar(c[i]);
-  
-  _putchar('\n');
-  
-
-  
-  return (0);
-  
+char c[8] = "_putchar";
+int i;
+for (i = 0; i < 8; ++i)
+_putchar(c[i]);
+_putchar('\n');
+return (0); 
 }
