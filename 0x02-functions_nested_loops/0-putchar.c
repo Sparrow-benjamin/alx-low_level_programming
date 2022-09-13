de1f8c74ea46b839525c59920ff818d8a07139ae
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-13 08:47:27 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+
+ * main - entry point
+
+ *
+
+ * Return: ALways Success(0)
+
+ */
+
+
+
+int main(void)
+  
+{
+  
+  char c[8] = "_putchar";
+  
+  int i;
+  
+
+  
+  for (i = 0; i < 8; ++i)
+    
+    _putchar(c[i]);
+  
+  _putchar('\n');
+  
+
+  
+  return (0);
+  
+}
