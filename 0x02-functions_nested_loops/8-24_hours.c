c71fc35034a63ee94b6694bd80d30e74e2817f4d
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-14 21:01:58 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,17 @@
  */
 void jack_bauer(void)
 {
-  int h, m;
-
-  for (h = 0; h < 24; h++)
-    {
-      for (m = 0; m < 60; m++)
-	{
-	  _putchar((h / 10) + '0');
-	  _putchar((h % 10) + '0');
-	  _putchar(':');
-	  _putchar((m / 10) + '0');
-	  _putchar((m % 10) + '0');
-	  _putchar('\n');
-	}
-    }
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
 }
