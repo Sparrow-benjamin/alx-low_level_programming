bb688973a0930f0f35c490ac34019f0a02df1734
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-14 20:17:42 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,10 @@
  */
 void print_alphabet(void)
 {
-  int n;
-
-  for (n = 'a'; n <= 'z'; n++)
-    {
-      _putchar(n);
-    }
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
  _putchar('\n');
 }
