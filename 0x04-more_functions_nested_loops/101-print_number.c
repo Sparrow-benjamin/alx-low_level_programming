d6217e2d1dd52bbaad23a3b4740a83f3f3993e70
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-16 02:01:30 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,15 @@
  */
 void print_number(int n)
 {
-  unsigned int num = n;
-  if (n < 0)
-    {
-      _putchar('-');
-      num = -num;
-    }
-  if (num > 9)
-    {
-      print_number(num / 10);
-    }
-  _putchar(num % 10 + '0');
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if (num > 9)
+{
+print_number(num / 10);
+}
+_putchar(num % 10 + '0');
 }
