4b6f838dca8d8c9898510bd813321fa973947715
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-16 01:41:56 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,21 @@
  */
 int main(void)
 {
-  int num;
-  while (num++ < 100)
-
-    if ((num % 3 == 0) && (num % 5 == 0))
-      printf("FizzBuzz ");
-
-    else if ((num % 3) == 0)
-      printf("Fizz ");
-
-    else if ((num % 5) == 0)
-      {
-	if (num != 100)
-	  printf("Buzz ");
-
-	else
-	  printf("Buzz");
-      }
-
-    else
-      printf("%d ", num);
-
-  printf("\n");
-  return (0);
+int num;
+while (num++ < 100)
+if ((num % 3 == 0) && (num % 5 == 0))
+printf("FizzBuzz ");
+else if ((num % 3) == 0)
+printf("Fizz ");
+else if ((num % 5) == 0)
+{
+if (num != 100)
+printf("Buzz ");
+else
+printf("Buzz");
+}
+else
+printf("%d ", num);
+printf("\n");
+return (0);
 }
