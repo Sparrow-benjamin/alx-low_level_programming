8dbad4b12ba167ed1ad14fa8ef5e89eb4fba6ce9
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-15 14:10:43 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-  int n, sum = 0;
-  for (n = 0; n < 1024; n++)
-    {
-      if ((n % 3) == 0 || (n % 5) == 0)
-	sum += n;
-    }
-  printf("%d\n", sum);
-  return (0);
+int n, sum = 0;
+for (n = 0; n < 1024; n++)
+{
+if ((n % 3) == 0 || (n % 5) == 0)
+sum += n;
+}
+printf("%d\n", sum);
+return (0);
 }
