bb5276a0972a3e07d2020288831a6cbb17058985
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-16 01:04:31 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,14 @@
  */
 void print_most_numbers(void)
 {
-  int number;
-  for (number = 48; number < 58; number++)
-    {
-      if ((number == 50) || (number == 52))
-	{
-	  continue;
-	}
-      _putchar(number);
-    }
-  _putchar(10);
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
 }
