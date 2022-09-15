8e34ace78c2209e3a6f846af683e331367a7d96d
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-16 01:48:17 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,19 @@
  */
 void print_triangle(int size)
 {
-  int inc1, inc2;
-
-  if (size > 0)
-    {
-      for (inc1 = 1; inc1 <= size; inc1++)
-	{
-	  for ((inc2 = size - inc1); inc2 > 0; inc2--)
-	    _putchar(' ');
-
-	  for (inc2 = 0; inc2 < inc1; inc2++)
-	    _putchar('#');
-
-	  if (inc1 == size)
-	    continue;
-
-	  _putchar('\n');
-	}
-    }
-  _putchar('\n');
+int inc1, inc2;
+if (size > 0)
+{
+for (inc1 = 1; inc1 <= size; inc1++)
+{
+for ((inc2 = size - inc1); inc2 > 0; inc2--)
+_putchar(' ');
+for (inc2 = 0; inc2 < inc1; inc2++)
+_putchar('#');
+if (inc1 == size)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
