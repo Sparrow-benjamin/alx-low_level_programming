450ee55ee66b342186748dc6a44284994d0f34f6
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-15 14:29:36 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,17 +7,15 @@
  */
 int main(void)
 {
-  unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-  while (n3 <= 4000000)
-    {
-      n3 = n1 + n2;
-      n1 = n2;
-      n2 = n3;
-
-      if ((n1 % 2) == 0)
-	sum += n1;
-
-    }
-  printf("%ld\n", sum);
-  return (0);
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
+{
+n3 = n1 + n2;
+n1 = n2;
+n2 = n3;
+if ((n1 % 2) == 0)
+sum += n1;
+}
+printf("%ld\n", sum);
+return (0);
 }
