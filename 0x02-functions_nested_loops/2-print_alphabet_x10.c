bd401ae0035494bda7767231684ef936bb309cd2
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-15 03:18:33 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * print_alphabet_x10 -  prints the alphabet x10
  *
- * Return: Always 0 (Succes)
  */
-void print_alphabet(void)
+
+void print_alphabet_x10(void)
 {
-int n;
-for (n = 'a'; n <= 'z'; n++)
-{
-_putchar(n);
-}
- _putchar('\n');
+  int alpha;
+  int alpha2;
+
+  for (alpha = 0; alpha <= 9; alpha++)
+    {
+      for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
+	{
+	  _putchar(alpha2);
+	}
+      _putchar('\n');
+    }
 }
