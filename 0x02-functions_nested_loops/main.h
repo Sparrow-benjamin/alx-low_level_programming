a3cea62290e667b6480d73f95102527bb6312cd0
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-15 03:03:28 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,29 @@
-#include "main.h"
-/**
- * add - a function that adds two integers and returns the result
- * @one: first number input
- * @two: second number input
- * Return: one + two
- */
-int add(int one, int two)
-{
-  return (one + two);
-}
+#define main_H_INCLUDED
+
+int _putchar(char);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+void num(int r, int n);
