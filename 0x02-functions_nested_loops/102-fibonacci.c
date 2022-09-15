e96eba1524040a87c55423728df7fa62f4745ffb
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-15 14:18:40 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,17 +10,14 @@ int main(void)
   unsigned long n1 = 0, n2 = 1, n3;
   for (inc = 0; inc < 50; inc++)
     {
-      n3 = n1 + n2;
-      printf("%lu", n3);
-      n1 = n2;
-      n2 = n3;
-
-      if (inc == 49)
-	printf("\n");
-      else
-	printf(", ");
-
-    }
-
-  return (0);
+n3 = n1 + n2;
+printf("%lu", n3);
+n1 = n2;
+n2 = n3;
+if (inc == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
 }
