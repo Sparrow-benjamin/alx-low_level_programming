9ee7bafdd970f67468d641a37196eb7fa2a8b898
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-20 12:15:27 -0700

    a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,35 +4,41 @@
 #include <time.h>
 
 /**
-* random_valid_passwords - check the code.
-* @n: an input integer.
-* Return: nothing.
-*/
-void random_valid_passwords(int len, int num){
-int temp;
-srand((unsigned int)(time(0)));
-while (num--)
+ * random_valid_passwords - check the code.
+ * @n: an input integer.
+ * Return: nothing.
+ */
+void random_valid_passwords(int len, int num)
 {
-temp = len;
-printf("\n");
-while (temp--)
-{
-putchar(rand() % 56 + 65);
-srand(rand());
+  int temp;
+
+  srand((unsigned int)(time(0)));
+
+  while (num--)
+    {
+      temp = len;
+      printf("\n");
+      while (temp--)
+	{
+	  putchar(rand() % 56 + 65);
+	  srand(rand());
+	}
+      temp = len;
+    }
+  printf("\n");
 }
-temp = len;
-}
-printf("\n");
-}
+
 /**
-* main - Entry point.
-*
-* Return: Always 0.
-*/
-int main(void)
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main()
 {
-int len = 14;
-int num = 1;
-random_valid_passwords(len, num);
-return (0);
+  int len = 14;
+  int num = 1;
+
+  random_valid_passwords(len, num);
+
+  return (0);
 }
