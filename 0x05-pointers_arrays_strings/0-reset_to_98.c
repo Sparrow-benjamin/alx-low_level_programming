80925bb776a39d0c8735f2bd54d55cc154b9735b
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-09-20 02:42:23 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates pointer value
+ *
+ * @n:  pointer used
+ *
+ * return: always 0
+ */
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
