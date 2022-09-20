63c91fb953d5755e13d38618a4d276900cf9ae87
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-20 18:30:48 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,21 +2,17 @@
 #include <stdio.h>
 
 /**
- * puts2 - check the code.
- * @str: an input string.
- * Return: nothing.
- */
+* puts2 - check the code.
+* @str: an input string.
+* Return: nothing.
+*/
 void puts2(char *str)
 {
-  int len = 0, i = 0;
-
-  while (str[len] != '\0')
-    len++;
-
-  len -= 1;
-
-  for (; i <= len; i += 2)
-    putchar(str[i]);
-
-  putchar('\n');
+int len = 0, i = 0;
+while (str[len] != '\0')
+len++;
+len -= 1;
+for (; i <= len; i += 2)
+putchar(str[i]);
+putchar('\n');
 }
