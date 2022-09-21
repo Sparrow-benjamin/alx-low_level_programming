509213855e843b973ae449ca7af3eb2266da0074
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-21 22:55:40 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters to uppercase
- * @s: string to change
- *
- * Return: address of s
- */
+* string_toupper - changes all lowercase letters to uppercase
+* @s: string to change
+*
+* Return: address of s
+*/
 char *string_toupper(char *s)
 {
-  int i = 0;
-
-  while (*(s + i))
-    {
-      if (*(s + i) >= 'a' && *(s + i) <= 'z')
-	*(s + i) -= 'a' - 'A';
-      i++;
-    }
-  return (s);
+int i = 0;
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
+}
+return (s);
 }
