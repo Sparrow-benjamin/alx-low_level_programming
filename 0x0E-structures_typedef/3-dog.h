771890fc9ab0b44c31be696f06f382e9e1f9cea0
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-10-11 12:04:16 +0300

    Update 3-dog.h

--- 0x0E-structures_typedef/3-dog.h
+++ 0x0E-structures_typedef/3-dog.h
@@ -2,15 +2,16 @@
 #define DOG_H
 
 /**
- *dog - structure for dog variables
- *@name:name
- *@age:age of dog
- *@owner:name of owner
- */
+*dog - structure for dog variables
+*@name:name
+*@age:age of dog
+*@owner:name of owner
+*/
 typedef struct dog
 {
-  char * name;
-  float age;
-  char *owner;
-} dog_t;
+char * name;
+float age;
+char *owner;
+}
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
