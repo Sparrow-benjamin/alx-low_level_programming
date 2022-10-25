84103d597bd0ead237432c5e8c18faf61c1d74b6
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-10-25 09:39:23 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *free_list - frees memory occupied by a list
- *@head:pointer to list
- *Return:void
- */
+*free_list - frees memory occupied by a list
+*@head:pointer to list
+*Return:void
+*/
 void free_list(list_t *head)
 {
-  list_t *tmp;
-
-  while (head)
-    {
-      tmp = head;
-      head = head->next;
-      free(tmp->str);
-      free(tmp);
-    }
+list_t *tmp;
+while (head)
+{
+tmp = head;
+head = head->next;
+free(tmp->str);
+free(tmp);
+}
 }
