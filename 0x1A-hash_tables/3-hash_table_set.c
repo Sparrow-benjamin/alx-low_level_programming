128b3785873cdfc2000ea43a7ff617f21e1054c8
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-01-06 20:06:18 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,43 +9,37 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
 hash_node_t *node;
 hash_node_t *new_node;
 unsigned long int index;
-
 if (ht == NULL || *key == '\n' || *value == '\n')
-	return (0);
-
+return (0);
 index = key_index((const unsigned char *)key, ht->size);
 node = ht->array[index];
-
 if (node == NULL)
 {
-	new_node = create_new_node(key, value);
-	if (new_node == NULL)
-		return (0);
-
-	ht->array[index] = new_node;
-	return (1);
+new_node = create_new_node(key, value);
+if (new_node == NULL)
+return (0);
+ht->array[index] = new_node;
+return (1);
 }
 
 /*If key exists, replace value*/
 while (node != NULL)
 {
-	if (strcmp(key, node->key) == 0)
-	{
-		free(node->value);
-		node->value = strdup(value);
-		return (1);
-	}
-	node = node->next;
+if (strcmp(key, node->key) == 0)
+{
+free(node->value);
+node->value = strdup(value);
+return (1);
+}
+node = node->next;
 }
 /*If key doesn't exist, create new node*/
 new_node = create_new_node(key, value);
 if (new_node == NULL)
-	return (0);
-
+return (0);
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
 return (1);
@@ -61,16 +55,12 @@ return (1);
 
 hash_node_t *create_new_node (const char *key, const char *value)
 {
-	hash_node_t *new_node;
-
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	return (new_node);
+hash_node_t *new_node;
+new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+return (NULL);
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = NULL;
+return (new_node);
 }
