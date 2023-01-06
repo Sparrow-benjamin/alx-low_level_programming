6e3aa53b41540681e2b18991ca6a621fc4685185
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-01-06 20:07:43 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,22 +7,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-
 hash_node_t *node;
 unsigned long int index;
-
 if (ht == NULL)
-	return (NULL);
-
+return (NULL);
 index = key_index((const unsigned char *) key, ht->size);
 node = ht->array[index];
-
 while (node != NULL)
 {
-	if (strcmp(node->key, key) == 0)
-		return (node->value);
-
-	node = node->next;
+if (strcmp(node->key, key) == 0)
+return (node->value);
+node = node->next;
 }
 return (NULL);
 }
