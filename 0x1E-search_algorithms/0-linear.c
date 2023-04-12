82ee69323cd1f08e0503e21e9a603537b4831f21
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-04-12 11:48:34 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,14 +14,12 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
-
-    for (size_t i = 0; i < size; i++) {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
-
-    return -1;
+if (array == NULL)
+return -1;
+for (size_t i = 0; i < size; i++) {
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return i;
+}
+return -1;
 }
